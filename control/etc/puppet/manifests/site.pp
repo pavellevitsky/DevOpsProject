9dc4fe8fae0bed8318799d6d688468044092e119
pavellevitsky <pavellev@inter.net.il>
2016-07-09 22:38:21 +0300

    Add site.pp on control node

---
+++ control/etc/puppet/manifests/site.pp
@@ -0,0 +1,35 @@
+node 'jenkins' {
+
+  include ntp
+
+  include java
+
+  include jenkins
+
+}
+
+node 'build-node' {
+
+  include ntp
+
+  include git
+
+  include java
+
+#  include maven
+
+}
+
+node 'webserver' {
+
+  include ntp
+
+#  include tomcat
+
+}
+
+node 'default' {
+
+   notify { "unknown machine": }
+
+}
